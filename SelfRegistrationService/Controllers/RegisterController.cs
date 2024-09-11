ea12b2b3d0f608ece7dbb924197ffe66c3b4b473
Wahlgreenen <holgerhsw01@gmail.com>
2024-09-11 12:35:14 +0200

    Add project files.

---
+++ SelfRegistrationService/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfRegistrationService
+{
+    [Route("api/")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private List<string> serverAdresses = new();
+
+        [HttpPost("Register")]
+        public IActionResult RegisterServer([FromBody] string address)
+        {
+            serverAdresses.Add(address);
+            return Ok("Server registered successfully.");
+        }
+
+        [HttpGet("GetServers"), Authorize]
+        public IActionResult GetServers()
+        {
+            if (serverAdresses.Count == 0)
+                return NotFound("No active gameservers.");
+
+            return Ok(serverAdresses);
+        }
+    }
+}
---
+++ SelfRegistrationService/Program.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SelfRegistrationService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("DANIATOKEN", "heElloooooooOoooooOoooooooooOoooooooooOooooooooooooooo????");
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication().AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("DANIATOKEN")!)),
+                };
+            });
+
+
+            var app = builder.Build();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
